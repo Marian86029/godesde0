3c20def09c9a885344f6c47cf678d21455d4e2d2
Mariano Romera <marianoromera@Marianos-MacBook-Pro.local>
2023-04-05 23:14:58 +0100

    Siguiente Commit

---
+++ ejecicios/ejercicios01.go
@@ -0,0 +1,23 @@
+package ejecicios
+
+import (
+	"strconv"
+)
+
+func PublicFunction(texto string) (int, string) {
+
+	num, err := strconv.Atoi(texto)
+
+	if err != nil {
+		return 0, "hubo error" + err.Error()
+	}
+
+	if num > 100 {
+		return num, "es mayor a 100"
+
+	} else {
+		return num, "es menor a 100"
+
+	}
+
+}
--- main.go
+++ main.go
@@ -3,13 +3,40 @@ package main
 import (
 	"fmt"
 
-	"github.com/Marian86029/godesde0/variables"
+	"github.com/Marian86029/godesde0/ejercicios"
 )
 
 func main() {
 
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
+	fmt.Println(texto)*/ // Primer Test
+
+	///    !!!!! IMPORTANT !!!!!   ///
+
+	// EXPLICACION IF, ELSE , ELSE IF, SWITCH //
+
+	/*if os := runtime.GOOS; os == "Linux" {
+		fmt.Println("esto no es windows", os)
+
+	} else {
+		fmt.Println("esto es windows")
+
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux":
+		fmt.Println("esto es Linux")
+	case "Darwin":
+		fmt.Println("esto es Darwin")
+	default:
+		fmt.Printf("%s\n", os)
+
+	}
+	*/
+
+	numero, texto := ejercicios.PublicFunction(500)
+	fmt.Println(numero)
 	fmt.Println(texto)
 
 }
