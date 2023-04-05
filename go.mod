dad8c5c293ec162ab0700fa26e5ccfdc463c2ce1
Mariano Romera <marianoromera@Marianos-MacBook-Pro.local>
2023-04-05 21:27:55 +0100

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Marian86029/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/Marian86029/godesde0/variables"
+)
+
+func main() {
+
+	/*fmt.Println("hola mundo")*/ // Primer Test
+
+	variables.MuestroEnteros()
+
+}
