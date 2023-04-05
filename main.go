b587f599f97a98538212a89aca8f0cea7282204a
Mariano Romera <marianoromera@Marianos-MacBook-Pro.local>
2023-04-05 21:54:53 +0100

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Marian86029/godesde0/variables"
 )
 
 func main() {
 
-	/*fmt.Println("hola mundo")*/ // Primer Test
-
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 
 }
